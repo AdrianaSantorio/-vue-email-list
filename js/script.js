2197fc9e8d7ba846b93fc85e3289db84da88c1df
AdrianaSantorio <adriana.santorio@gmail.com>
2022-02-08 01:29:18 +0100

    added get random mail function

--- js/script.js
+++ js/script.js
@@ -6,7 +6,20 @@ Vue.config.devtools = true;
 const root = new Vue({
     el:'#root',
     data: {
-
+        mails:[],
+        itemsNumber: 10,
+    },
+    methods: {
+        getRandomEmail() {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(res => {
+                this.mails.push(res.data.response);
+            });
+        },
+    },
+    mounted(){
+        for (let i = 0; i < this.itemsNumber; i++) {
+            this.getRandomEmail()
+        }
     },
 
 })
